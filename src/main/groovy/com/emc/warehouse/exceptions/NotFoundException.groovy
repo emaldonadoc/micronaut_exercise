fb495d34d231a3afe3a8269e8514a5003825865f
Emmanuel Maldonado <emmanuel@kubofinanciero.com>
2020-09-17 15:03:27 -0500

    Add jacoco test result

--- src/main/groovy/com/emc/warehouse/exceptions/NotFoundException.groovy
+++ src/main/groovy/com/emc/warehouse/exceptions/NotFoundException.groovy
@@ -1,8 +1,10 @@
 package com.emc.warehouse.exceptions
 
+import groovy.transform.CompileStatic
 import io.micronaut.http.HttpStatus
 import io.micronaut.http.exceptions.HttpStatusException
 
+@CompileStatic
 class NotFoundException extends HttpStatusException {
   NotFoundException(String message) {
     super(HttpStatus.NOT_FOUND, message)
--- src/main/groovy/com/emc/warehouse/exceptions/SkuException.groovy
+++ src/main/groovy/com/emc/warehouse/exceptions/SkuException.groovy
@@ -1,8 +1,10 @@
 package com.emc.warehouse.exceptions
 
+import groovy.transform.CompileStatic
 import io.micronaut.http.HttpStatus
 import io.micronaut.http.exceptions.HttpStatusException
 
+@CompileStatic
 class SkuException extends HttpStatusException {
   SkuException(String message) {
     super(HttpStatus.CONFLICT, message)
